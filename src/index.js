0132d86bcc8132923b0a10430c5b6280b940f6a2
Oscar Barajas <oscar@arepa.dev>
2020-08-13 17:41:07 -0500

    asd

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reducer from './reducers';
 
 import initialState from './initialState';
 
+const store = createStore(reducer, initialState);
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
